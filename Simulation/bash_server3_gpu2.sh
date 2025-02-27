439facd4129c06deacbe6dd917ab8432e2cf3e8d
Yichao Cai <yichao.cai@adelaide.edu.au>
2025-02-27 22:26:52 +1030

    update code

---
+++ Simulation/bash_server3_gpu2.sh
@@ -0,0 +1,16 @@
+echo "run numerical simulaton..."
+
+CUDA_VISIBLE_DEVICES=2 python main_numeric.py --model-id "multidist_ind_drop3_tr1" --theta-value 847 --multi-distribution --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=2 python main_numeric.py --model-id "multidist_ind_drop3_tr2" --theta-value 847 --multi-distribution --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=2 python main_numeric.py --model-id "multidist_ind_drop3_tr3" --theta-value 847 --multi-distribution --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=2 python main_numeric.py --model-id "multidist_ind_drop1_tr1" --theta-value 1012 --multi-distribution --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=2 python main_numeric.py --model-id "multidist_ind_drop1_tr2" --theta-value 1012 --multi-distribution --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=2 python main_numeric.py --model-id "multidist_ind_drop1_tr3" --theta-value 1012 --multi-distribution --mlp-eval
+wait
+
+echo "training end."
