f3cd353b5c6d4b4e3a3aec04fda4713d5d6f0f81
Yichao Cai <yichao.cai@adelaide.edu.au>
2025-03-02 21:04:56 +1030

    update

---
+++ bash_server3_gpu3.sh
@@ -0,0 +1,10 @@
+echo "run numerical simulaton..."
+
+CUDA_VISIBLE_DEVICES=0 python main_numeric.py --model-id "dep_drop2_tr1" --theta-value 967 --causal-dependence --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=0 python main_numeric.py --model-id "dep_drop1_tr2" --theta-value 1012 --causal-dependence --mlp-eval
+wait
+CUDA_VISIBLE_DEVICES=0 python main_numeric.py --model-id "dep_drop1_tr3" --theta-value 1012 --causal-dependence --mlp-eval
+wait
+
+echo "training end."
