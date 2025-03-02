f3bf70b484d680f5636b4a446720c072b4e10c62
Yichao Cai <yichao.cai@adelaide.edu.au>
2025-03-02 21:14:36 +1030

    update

--- bash_server2_gpu1.sh
+++ bash_server2_gpu1.sh
@@ -1,8 +1,8 @@
 echo "run numerical simulaton..."
-
-CUDA_VISIBLE_DEVICES=1 python main_numeric.py --model-id "ind_perturb2_tr1" --beta-value 10 --mlp-eval
+CUDA_VISIBLE_DEVICES=1 python main_numeric.py --model-id "dep_perturb1_tr1" --beta-value 0 --causal-dependence --mlp-eval
 wait
-CUDA_VISIBLE_DEVICES=1 python main_numeric.py --model-id "ind_perturb1_tr2" --beta-value 0 --mlp-eval
+CUDA_VISIBLE_DEVICES=1 python main_numeric.py --model-id "dep_perturb1_tr2" --beta-value 0 --causal-dependence --mlp-eval
 wait
+CUDA_VISIBLE_DEVICES=1 python main_numeric.py --model-id "dep_perturb1_tr3" --beta-value 0 --causal-dependence --mlp-eval
 
 echo "training end."
