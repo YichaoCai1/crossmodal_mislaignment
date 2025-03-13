f7b3d2ef69f8e10963162a21249bbf9b36096a7f
Yichao Cai <yichao.cai@adelaide.edu.au>
2025-03-13 19:58:33 +1030

    update code

--- bash_s3_gpu1_mpi3d.sh
+++ bash_s3_gpu1_mpi3d.sh
@@ -1,14 +1,14 @@
 echo "run numerical simulaton..."
 
-CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_1_tr3" --bias-type "perturbation" --bias-id 1
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_1_tr3" --bias-type "perturbation" --bias-id 1
 wait
-CUDA_VISIBLE_DEVICES=3 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_2_tr1" --bias-type "perturbation" --bias-id 2
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_2_tr1" --bias-type "perturbation" --bias-id 2
 wait
-CUDA_VISIBLE_DEVICES=3 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_2_tr2" --bias-type "perturbation" --bias-id 2
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_2_tr2" --bias-type "perturbation" --bias-id 2
 wait
-CUDA_VISIBLE_DEVICES=3 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_2_tr3" --bias-type "perturbation" --bias-id 2
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_2_tr3" --bias-type "perturbation" --bias-id 2
 wait
-CUDA_VISIBLE_DEVICES=3 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_3_tr1" --bias-type "perturbation" --bias-id 3
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_perturbation_3_tr1" --bias-type "perturbation" --bias-id 3
 wait
 
 echo "training end."
