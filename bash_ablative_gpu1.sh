83316a73bbc84660af5ee584c5faf7fb8ac1b69c
Yichao Cai <yichao.cai@adelaide.edu.au>
2025-03-14 18:06:44 +1030

    update

--- bash_ablative_gpu1.sh
+++ bash_ablative_gpu1.sh
@@ -1,10 +1,10 @@
 echo "run numerical simulaton..."
 
-CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc2_tr1" --bias-type "selection" --bias-id 1 --encoding-size 2
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc2_tr1" --bias-type "selection" --bias-id 0 --encoding-size 2
 wait
-CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc2_tr2" --bias-type "selection" --bias-id 1 --encoding-size 2
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc2_tr2" --bias-type "selection" --bias-id 0 --encoding-size 2
 wait
-CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc2_tr3" --bias-type "selection" --bias-id 1 --encoding-size 2
+CUDA_VISIBLE_DEVICES=1 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc2_tr3" --bias-type "selection" --bias-id 0 --encoding-size 2
 wait
 
 
--- bash_ablative_gpu2.sh
+++ bash_ablative_gpu2.sh
@@ -1,10 +1,10 @@
 echo "run numerical simulaton..."
 
-CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc4_tr1" --bias-type "selection" --bias-id 1 --encoding-size 4
+CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc4_tr1" --bias-type "selection" --bias-id 0 --encoding-size 4
 wait
-CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc4_tr2" --bias-type "selection" --bias-id 1 --encoding-size 4
+CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc4_tr2" --bias-type "selection" --bias-id 0 --encoding-size 4
 wait
-CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc4_tr3" --bias-type "selection" --bias-id 1 --encoding-size 4
+CUDA_VISIBLE_DEVICES=2 python main_mpi3dreal.py --datapath "./data/MPI3d_real_complex/" --model-id "mpi3d_selection_0_enc4_tr3" --bias-type "selection" --bias-id 0 --encoding-size 4
 wait
 
 
