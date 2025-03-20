bcae79251e778d41e532f503f538f56ccfc37349
Yichao Cai <yichao.cai@adelaide.edu.au>
2025-03-21 00:14:08 +1030

    update code

--- bash_c3did_gpu1_add.sh
+++ bash_c3did_gpu1_add.sh
@@ -1,10 +1,10 @@
 echo "run numerical simulaton..."
 
-CUDA_VISIBLE_DEVICES=1 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_selections_enc2_0_tr1" --bias-type "selections" --bias-id 0 --encoding-size 1
+CUDA_VISIBLE_DEVICES=1 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_selections_0_enc1_tr1" --bias-type "selections" --bias-id 0 --encoding-size 1
 wait
-CUDA_VISIBLE_DEVICES=1 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_selections_enc2_0_tr2" --bias-type "selections" --bias-id 0 --encoding-size 1
+CUDA_VISIBLE_DEVICES=1 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_selections_0_enc1_tr2" --bias-type "selections" --bias-id 0 --encoding-size 1
 wait
-CUDA_VISIBLE_DEVICES=1 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_selections_enc2_0_tr3" --bias-type "selections" --bias-id 0 --encoding-size 1
+CUDA_VISIBLE_DEVICES=1 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_selections_0_enc1_tr3" --bias-type "selections" --bias-id 0 --encoding-size 1
 wait
 
 echo "training end."
--- bash_c3did_gpu2_add.sh
+++ bash_c3did_gpu2_add.sh
@@ -1,10 +1,10 @@
 echo "run numerical simulaton..."
 
-CUDA_VISIBLE_DEVICES=2 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_perturbations_6_tr1" --bias-type "perturbations" --bias-id 6 --encoding-size 2
+CUDA_VISIBLE_DEVICES=2 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_perturbations_6_enc2_tr1" --bias-type "perturbations" --bias-id 6 --encoding-size 2
 wait
-CUDA_VISIBLE_DEVICES=2 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_perturbations_6_tr2" --bias-type "perturbations" --bias-id 6 --encoding-size 2
+CUDA_VISIBLE_DEVICES=2 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_perturbations_6_enc2_tr2" --bias-type "perturbations" --bias-id 6 --encoding-size 2
 wait
-CUDA_VISIBLE_DEVICES=2 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_perturbations_6_tr3" --bias-type "perturbations" --bias-id 6 --encoding-size 2
+CUDA_VISIBLE_DEVICES=2 python main_causal3dident.py --datapath "data/Causal3DIDent" --model-id "C3dId/c3did_perturbations_6_enc2_tr3" --bias-type "perturbations" --bias-id 6 --encoding-size 2
 wait
 
 echo "training end."
